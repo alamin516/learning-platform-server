7f6c7b6d100c529c5ab4745cc719ae86f2897694
Md Alamin <alaminduc516@gmail.com>
2022-10-27 13:20:08 +0600

    Courses data added

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./Data/courses.json');
 
+app.get('/', (req, res)=>{
+    res.send('Server is comming');
+})
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
+})
 
 
 app.listen(port, ()=>{
