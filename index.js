462a378006135dcb2115976008d242a74cc62267
Md Alamin <alaminduc516@gmail.com>
2022-10-27 17:40:47 +0600

    Single course details link created

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ app.use(cors());
 
 const courses = require('./Data/courses.json');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Server is comming');
 })
 
-app.get('/courses', (req, res)=>{
+app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourse = courses.find(c => c.id === id) || {};
+    res.send(singleCourse);
+    console.log(singleCourse)
 
-app.listen(port, ()=>{
+})
+
+
+app.listen(port, () => {
     console.log('Server is running', port);
-}) 
+})
 
 
